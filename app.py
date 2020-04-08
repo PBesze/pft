cf5742d30725d4d79bec9be6adccf1ca9ac6656c
Peter Besze <pbesze@gmail.com>
2020-04-08 09:12:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
 
 @app.route('/welcome')
 def hello():
-    return '2nd line on the page'
+    return 'Wlecome link works'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
